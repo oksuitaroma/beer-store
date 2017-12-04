5bad039c21dd6f0ec3546815b59796911db44a84
OksiutaRoman <romafenomen@gmail.com>
2017-12-05 00:37:02 +0200

    Google map API

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function initMap() {
+  var uluru = {lat: 50.391503, lng: 30.626270};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
